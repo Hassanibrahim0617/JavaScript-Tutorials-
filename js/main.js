be059c5ef4d3672365e2e2a0e9209f2a5a69b30b
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2023-07-02 21:00:11 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// let fname ='hassan';
+// console.dir(fname.slice(3))
+
+ let generate = `Generate Lorem Ipsum placeholder text. Select the number of characters, 
+ words, sentences or paragraphs, and hit generate!`;
+ console.log(generate.length);
+ console.log(generate.toUpperCase());
+ console.log(generate.toLowerCase());
+ console.log(generate.slice(20,105));
+ console.dir(generate.fontcolor('green'));
+ console.dir(generate.fontsize('50'));
+
